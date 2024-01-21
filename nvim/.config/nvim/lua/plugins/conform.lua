9f6f25ebc7b1268ef66842796f3255a627a03984
Shawnbreak <thezhxin@gmail.com>
2024-01-21 16:09:29 +0800

    add json formater

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,26 +1,27 @@
 return {
-	"stevearc/conform.nvim",
-	opts = {},
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				-- lua = { "stylua" },
-				-- Conform will run multiple formatters sequentially
-				python = { "isort", "black" },
-				-- Use a sub-list to run only the first available formatter
-				javascript = { { "prettierd", "prettier" } },
-				c = { "clang-format" },
-			},
-			format_on_save = {
-				-- these options will be passed to conform.format()
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
-		})
-		-- vim.api.nvim_create_autocmd("BufWritePre", {
-		--   pattern = "*",
-		--   callback = function(args)
-		--     require("conform").format({ bufnr = args.buf })
-		--   end,
-	end,
+  "stevearc/conform.nvim",
+  opts = {},
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        -- lua = { "stylua" },
+        -- Conform will run multiple formatters sequentially
+        python = { "isort", "black" },
+        -- Use a sub-list to run only the first available formatter
+        javascript = { { "prettierd", "prettier" } },
+        c = { "clang-format" },
+        json = { "jq" }
+      },
+      format_on_save = {
+        -- these options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+    })
+    -- vim.api.nvim_create_autocmd("BufWritePre", {
+    --   pattern = "*",
+    --   callback = function(args)
+    --     require("conform").format({ bufnr = args.buf })
+    --   end,
+  end,
 }
