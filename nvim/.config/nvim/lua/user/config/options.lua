cbe9f23f24406956543a808ee9b1711b7d13e4d1
rocky <rocky@Linx>
2024-02-18 16:29:09 +0800

    update nvim

--- nvim/.config/nvim/lua/user/config/options.lua
+++ nvim/.config/nvim/lua/user/config/options.lua
@@ -23,6 +23,17 @@ vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldenable = false
 
 vim.cmd([[colorscheme onedark]])
+vim.cmd([[hi Statement gui=bold]])
+vim.cmd([[hi Type gui=bold]])
+vim.cmd([[hi Keyword gui=bold]])
+vim.cmd([[hi Special gui=bold]])
+vim.cmd([[hi Operator gui=bold]])
+vim.cmd([[hi @keyword gui=bold]])
+vim.cmd([[hi @keyword.conditional gui=bold]])
+vim.cmd([[hi @keyword.repeat gui=bold]])
+vim.cmd([[hi @type gui=bold]])
+vim.cmd([[hi @type.builtin gui=bold]])
+
 vim.opt.laststatus = 3
 
 vim.api.nvim_set_hl(0, 'WinSeparator', { fg = 'gray', bold = true })
