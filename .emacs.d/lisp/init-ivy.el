bb882a96f071e48ff7fcdae3c281ca320f90ebbf
shawnbreak <thezhxin@gmail.com>
2024-08-11 23:33:17 +0800

    update

--- .emacs.d/lisp/init-ivy.el
+++ .emacs.d/lisp/init-ivy.el
@@ -2,13 +2,13 @@
 ;;; Commentary:
 ;;; Code:
 (use-package ivy
+  :ensure t
   :diminish
-  :bind (("C-s" . swiper)
-	 :map ivy-minibuffer-map
-	 ("TAB" . ivy-alt-done)
-	 ("C-l" . ivy-alt-done)
-	 ("C-j" . ivy-next-line)
-	 ("C-k" . ivy-previous-line)
+  :bind (("C-s" . swiper-thing-at-point)
+	 ("C-c C-r" . ivy-resume)
+;;	 :map ivy-minibuffer-map
+;;	 ("TAB" . ivy-alt-done)
+;;	 ("C-l" . ivy-alt-done)
 	 :map ivy-switch-buffer-map
 	 ("C-k" . ivy-previous-liine)
 	 ("C-l" . ivy-done)
@@ -17,6 +17,8 @@
 	 ("C-k" . ivy-previous-line)
 	 ("C-d" . ivy-reverse-i-search-kill))
   :config
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-count-format "(%d/%d) ")
   (ivy-mode 1))
 
 
@@ -26,12 +28,15 @@
 
 (use-package counsel
   :ensure t
+  :diminish
   :init
   :bind (("M-x" . counsel-M-x)
 	 ("C-x b" . counsel-ibuffer)
 	 ("C-x C-f" . counsel-find-file)
 	 :map minibuffer-local-map
-	 ("C-r" . counsel-minibuffer-history)))
+	 ("C-r" . counsel-minibuffer-history))
+  :config
+  (counsel-mode 1))
 
 (global-set-key (kbd "C-M-j") 'counsel-switch-buffer)
 
