65dbf84f92913fef976e9d15d48c5e8903efa7bf
shawnbreak <thezhxin@gmail.com>
2025-01-14 22:56:29 +0800

    add markdown number heading functions

--- .emacs.d/lisp/init-markdown.el
+++ .emacs.d/lisp/init-markdown.el
@@ -22,5 +22,42 @@
 
 
 
+(defun my/markdown-number-headings ()
+  "Automatically number headings in Markdown while preserving the '#' symbols."
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (let ((counters (make-vector 6 0)))
+      (while (re-search-forward "^\\(#+\\) \\(.*\\)$" nil t)
+        (let* ((level (- (match-end 1) (match-beginning 1)))
+               (counter (1+ (aref counters (1- level)))))
+	  (message "head level: %s, counter: %s" level counter)
+          (aset counters (1- level) counter)
+          (dotimes (i (- 6 level))
+            (aset counters (+ i level) 0))
+          (replace-match
+           (format "%s %s %s"
+                   (match-string 1)
+                   (mapconcat #'number-to-string
+                              (cl-loop for i from 0 below level
+				       collect (aref counters i))
+                              ".")
+                   (match-string 2))
+           t t))))))
+
+
+(defun my/markdown-remove-heading-numbers ()
+  "Remove numbering from headings in Markdown."
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (while (re-search-forward "^\\(#+\\) \\([0-9]+\\.*\\)+ *\\(.*\\)$" nil t)
+      (replace-match
+       (format "%s %s"
+               (match-string 1)
+               (match-string 3))
+
+       t t))))
+
 (provide 'init-markdown)
 ;;; init-markdown.el ends here
